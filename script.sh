5d39184cc8c44db8c0c2738b19fa60c2a1b40c45
yarl baudig <yar-baudig@mailoo.org>
2014-03-29 12:51:06 +0100

    ajout script pour l'exécution automatisée (pas fini pas testé)

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ls_with_dir () {
+    sed -e "s:.*:$1&:" <(ls $1) 
+}
+
+for file in $( ls_with_dir "A/" ; ls_with_dir "B/" )
+do
+    basefile=$1$(basename $file)
+    ( exec1="./projet $file PERSONNAL_ALGORITHM > $basefile.palog" ;
+    echo $exec1 ; 
+    eval $exec1 ) &
+    ( exec2="./projet $file JOHN_TROT > $basefile.jtlog" ;
+    echo $exec2 ;
+    eval $exec2 )
+done
